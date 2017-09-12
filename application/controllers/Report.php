694b7ca57852af727701bccba8c181e240bcda4c
bluelife <thebulelife@163>
2017-09-12 14:20:26 +0800

    ....

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -13,6 +13,7 @@ class Report extends ADLINKX_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->initialization();
+		$this->load->model('store_model','store');
 	}
 
 	public function index() {
@@ -36,6 +37,9 @@ class Report extends ADLINKX_Controller {
 	}
 
 	public function lists() {
-		$this->display('report/launch_list.html');
+		$store = $this->store->get_all(array('own_id' => $this->session->userdata('uid'), 'is_del'=> '0'));
+		// var_dump($store);
+		$this->assign('store',$store);
+		$this->display('report/report.html');
 	}
 }
