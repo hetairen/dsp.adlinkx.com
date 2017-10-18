d8b8360f1c717b9dfc1912b68e4ca6fa5d9ec064
bluelifelee <thebulelife@163.com>
2017-10-18 13:56:44 +0800

    ....

--- application/models/Creative_model.php
+++ application/models/Creative_model.php
@@ -16,14 +16,14 @@ class Creative_model extends ADLINKX_Model {
 		parent::__construct();
 		$this->db = $this->get_database('aliyun');
 		$this->table_name = 'diy_ad_task';
-		$this->load->model('store_model','store');
-		$this->load->model('launch_model','launch');
-		$this->load->model('strategy_model','strategy');
-		$this->load->model('user_model','user');
-		$this->load->model('diy_board_model','diy_board');
+		$this->load->model('store_model', 'store');
+		$this->load->model('launch_model', 'launch');
+		$this->load->model('strategy_model', 'strategy');
+		$this->load->model('user_model', 'user');
+		$this->load->model('diy_board_model', 'diy_board');
 	}
 
-	public function add($data = array()){
+	public function add($data = array()) {
 		// var_dump($data);
 		$ad_task_id = $this->get_seq_id();
 		// var_dump($ad_task_id);
@@ -72,43 +72,44 @@ class Creative_model extends ADLINKX_Model {
 			'status' => 0,
 			'pic_size' => $data['pic_width'] . '*' . $data['pic_height'],
 			'gxb_monitor_url' => isset($data['monitor_url']) ? $data['monitor_url'] : '',
-			'mz_audit_stat' => 1
+			'mz_audit_stat' => 1,
 		));
 		return $query && $this->db->affected_rows() > 0 ? $ad_task_id : false;
 	}
 
-	public function get($where = array(),$fields = '*'){
+	public function get($where = array(), $fields = '*') {
 		$this->db->select($fields);
 		$this->db->from($this->table_name);
 		$this->db->where($where);
 		$query = $this->db->get();
-		return $query && $query->num_rows() > 0 ? $query->row_array() : array() ;
+		return $query && $query->num_rows() > 0 ? $query->row_array() : array();
 	}
 
-	public function lists($where = array(), $offset = 0, $num = 20, $key = 'id', $stor = 'desc', $fields = '*', &$count){
-		$count_sql = "SELECT COUNT(*) AS `count` FROM (`diy_ad_task` AS `dat`) LEFT JOIN `store` AS `s` ON s.shop_id = dat.shop_id LEFT JOIN `dsp_stats_ad_task` AS `ds` ON ds.date BETWEEN '2017-09-17' AND '2017-09-17' AND ds.ad_task_id = dat.borad_id WHERE `dat`.`uid` = '".$where['uid']."' ".(isset($where['shop_id']) ? "AND `dat`.`shop_id`=".$where['shop_id']: '')." AND `dat`.`plat_id` = '134' ".(isset($where['unit_id']) ? "AND `dat`.`unit_id`=".$where['unit_id']: '')." AND `dat`.`is_del` = '0' GROUP BY `dat`.`borad_id` ORDER BY `dat`.`status`";
-		$count = $this->db->query($count_sql)->result_array()[0]['count'];
-		$sql = "SELECT `dat`.`id`,`dat`.`borad_id`, `dat`.`borad_name`, `dat`.`unit_id`, `dat`.`unit_name`, `dat`.`plan_id`, `dat`.`plan_name`, `dat`.`shop_id`, `dat`.`plat_id`, `dat`.`plat_name`, `dat`.`uid`, `dat`.`status`, `dat`.`pic_path`,`dat`.`pic_size`,`dat`.`borad_url`, `s`.`shop_title`, IFNULL(SUM(`ds`.`pv`), 0) AS 'pv', IFNULL(SUM(`ds`.`click`), 0) AS 'click', IFNULL(SUM(`ds`.`charge`), 0) AS 'charge', IFNULL(SUM(`ds`.`click`) / SUM(`ds`.`pv`) * 100, 0) AS `ctr`, IFNULL(SUM(`ds`.`charge`) / SUM(`ds`.`click`), 0) AS `click_cost` FROM (`diy_ad_task` AS `dat`) LEFT JOIN `store` AS `s` ON s.shop_id = dat.shop_id LEFT JOIN `dsp_stats_ad_task` AS `ds` ON ds.date BETWEEN '2017-09-17' AND '2017-09-17' AND ds.ad_task_id = dat.borad_id WHERE `dat`.`uid` = '".$where['uid']."' ".(isset($where['shop_id']) ? "AND `dat`.`shop_id`=".$where['shop_id']: '')." AND `dat`.`plat_id` = '134' ".(isset($where['unit_id']) ? "AND `dat`.`unit_id`=".$where['unit_id']: '')." AND `dat`.`is_del` = '0' GROUP BY `dat`.`borad_id` ORDER BY `dat`.`status` DESC LIMIT ".intval(($offset-1)/$num).",".$num;
+	public function lists($where = array(), $offset = 0, $num = 20, $key = 'id', $stor = 'desc', $fields = '*', &$count) {
+		$count_sql = "SELECT COUNT(*) AS `count` FROM (`diy_ad_task` AS `dat`) LEFT JOIN `store` AS `s` ON s.shop_id = dat.shop_id LEFT JOIN `dsp_stats_ad_task` AS `ds` ON ds.date BETWEEN '2017-09-17' AND '2017-09-17' AND ds.ad_task_id = dat.borad_id WHERE `dat`.`uid` = '" . $where['uid'] . "' " . (isset($where['shop_id']) ? "AND `dat`.`shop_id`=" . $where['shop_id'] : '') . " AND `dat`.`plat_id` = '134' " . (isset($where['unit_id']) ? "AND `dat`.`unit_id`=" . $where['unit_id'] : '') . " AND `dat`.`is_del` = '0' GROUP BY `dat`.`borad_id` ORDER BY `dat`.`status`";
+		$count_query = $this->db->query($count_sql);
+		$count = $count_query && $count_query->num_rows() > 0 ? $count_query->result_array()[0]['count'] : 0;
+		$sql = "SELECT `dat`.`id`,`dat`.`borad_id`, `dat`.`borad_name`, `dat`.`unit_id`, `dat`.`unit_name`, `dat`.`plan_id`, `dat`.`plan_name`, `dat`.`shop_id`, `dat`.`plat_id`, `dat`.`plat_name`, `dat`.`uid`, `dat`.`status`, `dat`.`pic_path`,`dat`.`pic_size`,`dat`.`borad_url`, `s`.`shop_title`, IFNULL(SUM(`ds`.`pv`), 0) AS 'pv', IFNULL(SUM(`ds`.`click`), 0) AS 'click', IFNULL(SUM(`ds`.`charge`), 0) AS 'charge', IFNULL(SUM(`ds`.`click`) / SUM(`ds`.`pv`) * 100, 0) AS `ctr`, IFNULL(SUM(`ds`.`charge`) / SUM(`ds`.`click`), 0) AS `click_cost` FROM (`diy_ad_task` AS `dat`) LEFT JOIN `store` AS `s` ON s.shop_id = dat.shop_id LEFT JOIN `dsp_stats_ad_task` AS `ds` ON ds.date BETWEEN '2017-09-17' AND '2017-09-17' AND ds.ad_task_id = dat.borad_id WHERE `dat`.`uid` = '" . $where['uid'] . "' " . (isset($where['shop_id']) ? "AND `dat`.`shop_id`=" . $where['shop_id'] : '') . " AND `dat`.`plat_id` = '134' " . (isset($where['unit_id']) ? "AND `dat`.`unit_id`=" . $where['unit_id'] : '') . " AND `dat`.`is_del` = '0' GROUP BY `dat`.`borad_id` ORDER BY `dat`.`status` DESC LIMIT " . intval(($offset - 1) / $num) . "," . $num;
 		$query = $this->db->query($sql);
-		return $query && $query->num_rows() > 0 ? $query->result_array() : array() ;
+		return $query && $query->num_rows() > 0 ? $query->result_array() : array();
 	}
 
-	public function update($data = array(), $where = array()){
+	public function update($data = array(), $where = array()) {
 		$this->db->where($where);
-		$query = $this->db->update($this->table_name,$data);
+		$query = $this->db->update($this->table_name, $data);
 		// var_dump($this->db->last_query());
-		return $query && $this->db->affected_rows() > 0 ? true: false;
+		return $query && $this->db->affected_rows() > 0 ? true : false;
 	}
 
-	public function delete($where = array()){
+	public function delete($where = array()) {
 		$FB = null;
-		for($i=0;$i<count($where);$i++){
+		for ($i = 0; $i < count($where); $i++) {
 			$FB = $this->update(array('is_del' => 1), $where[$i]);
 		}
 		return $FB;
 	}
 
-	public function edit_creative($data,$where){
+	public function edit_creative($data, $where) {
 		$creative_data = array(
 			'borad_name' => $data['borad_name'],
 			'borad_url' => $data['url'],
@@ -124,18 +125,18 @@ class Creative_model extends ADLINKX_Model {
 		return $this->update($creative_data, $where);
 	}
 
-	public function build_where($where = array()){
+	public function build_where($where = array()) {
 		$tmp = '';
-		foreach($where AS $k => $v){
-			if( $v != ''){
-				if($k == 'borad_name'){
-					$tmp .= '`' . $k . '`' . ' like "%' . $v .'%" and ';
-				}else{
-					$tmp .= '`' . $k . '`' . '=' . $v .' and ';
+		foreach ($where AS $k => $v) {
+			if ($v != '') {
+				if ($k == 'borad_name') {
+					$tmp .= '`' . $k . '`' . ' like "%' . $v . '%" and ';
+				} else {
+					$tmp .= '`' . $k . '`' . '=' . $v . ' and ';
 				}
-				
+
 			}
 		}
-		return $tmp == '' ? '' : 'where '. substr($tmp,0,intval(strlen($tmp)-5));
+		return $tmp == '' ? '' : 'where ' . substr($tmp, 0, intval(strlen($tmp) - 5));
 	}
 }
