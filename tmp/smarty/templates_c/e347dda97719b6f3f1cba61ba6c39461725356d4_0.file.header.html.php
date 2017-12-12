aa79dbc98f95177a1b30e5f40d5b88d5e822c564
bluelifelee <thebulelife@163.com>
2017-12-12 15:17:37 +0800

    .....

---
+++ tmp/smarty/templates_c/e347dda97719b6f3f1cba61ba6c39461725356d4_0.file.header.html.php
@@ -0,0 +1,72 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-12-11 13:57:04
+  from "C:\web\www\dsp.adlinkx.com\application\views\common\header.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5a2e1e30d0e594_23782716',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e347dda97719b6f3f1cba61ba6c39461725356d4' => 
+    array (
+      0 => 'C:\\web\\www\\dsp.adlinkx.com\\application\\views\\common\\header.html',
+      1 => 1512097562,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5a2e1e30d0e594_23782716 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="keywords" content="领效,AdlinkxDSP,领效DSP,大数据平台">
+    <meta name="description" content="杭州领效科技有限公司 领效DSP（AdlinkxDSP）作为一家创新型电商大数据营销企业，几年来依托专业的技术实力及持续不断的产品创新，已成功跻身为数字营销领域的先驱者。其自有的大数据平台每日处理用户曝光量累计可达100亿，全面覆盖4.3亿在线网民，为电商客户提供广告大数据营销、行业买家/卖家人群分析、舆情监测、数据增值等多项服务。">
+    <meta name="company" content="杭州领效科技有限公司">
+    <meta name="author" content="bluelife" />
+    <meta name="email" content="thebulelife@outlook.com" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/png">
+    <title>领效DSP</title>
+    <link href="/resources/css/style.default.css" rel="stylesheet">
+    <!-- <link href="/resources/css/animate.css" rel="stylesheet"> -->
+    <link rel="stylesheet" type="text/css" href="/resources/css/public.css" />
+    <link rel="stylesheet" type="text/css" href="/resources/css/qapTcha.jquery_taobao.css" />
+    <link rel="stylesheet" type="text/css" href="/resources/css/loading.css" />
+    <?php echo '<script'; ?>
+ type="text/javascript" src="/resources/js/libarays/jquery-1.8.2.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="/resources/js/libarays/jquery-ui-1.8.23.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="/resources/js/libarays/jquery-ui-touch-plugin.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="/resources/js/libarays/echarts.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="/resources/js/libarays/qapTcha.jquery_taobao.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="/resources/js/libarays/zepto.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="/resources/js/libarays/md5.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="/resources/js/public.js"><?php echo '</script'; ?>
+>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+  <?php echo '<script'; ?>
+ src="/resources/js/libarays/html5shiv.js"><?php echo '</script'; ?>
+>
+  <?php echo '<script'; ?>
+ src="/resources/js/libarays/respond.min.js"><?php echo '</script'; ?>
+>
+  <![endif]--><?php }
+}
